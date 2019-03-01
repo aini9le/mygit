09f494fd411f60b1a4bc4315e71b7be94a3d49d1
aini9le <aini9le@126.com>
2019-03-01 17:38:47 +0800

    1

--- cloud-common/common-entity/src/main/java/com/lii/cloud/common/entity/admin/vo/UserVo.java
+++ cloud-common/common-entity/src/main/java/com/lii/cloud/common/entity/admin/vo/UserVo.java
@@ -1,8 +1,22 @@
 package com.lii.cloud.common.entity.admin.vo;
 
+import java.util.List;
+
 import com.lii.cloud.common.entity.admin.po.User;
 import com.lii.cloud.common.tools.result.ResponseInfo;
 
 public class UserVo extends User implements ResponseInfo{
+	
+	private List<User> list;
+
+	public List<User> getList() {
+		return list;
+	}
+
+	public void setList(List<User> list) {
+		this.list = list;
+	}
+	
+	
 
 }
--- cloud-services/admin-services/src/main/java/com/lii/cloud/admin/controller/UserController.java
+++ cloud-services/admin-services/src/main/java/com/lii/cloud/admin/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.lii.cloud.admin.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
@@ -11,8 +14,10 @@ import com.lii.cloud.admin.services.UserService;
 import com.lii.cloud.admin.tools.UIPageInfo;
 import com.lii.cloud.common.base.concroller.BaseConcroller;
 import com.lii.cloud.common.entity.admin.po.User;
+import com.lii.cloud.common.entity.admin.vo.UserVo;
 import com.lii.cloud.common.entity.base.dto.BasePageInfoExampleDTO;
 import com.lii.cloud.common.tools.result.ResultBody;
+import com.lii.cloud.common.tools.result.defaults.DefaultObjectResponseInfo;
 
 /**
  * 用户
@@ -38,5 +43,26 @@ public class UserController extends BaseConcroller<User>{
 		ui.setTotal(page.getTotal());
 		return ResultBody.success("",ui);
 	}
+	
+	/**
+	 * 测试主单和明细
+	 * @param exDto
+	 * @return
+	 */
+	@RequestMapping(value = "testMainAndDEtail")
+	@ResponseBody
+	public ResultBody testMainAndDEtail(BasePageInfoExampleDTO exDto) {
+		User user1 = new User();
+		User user2 = new User();
+		List list = new ArrayList();
+		list.add(user1);
+		list.add(user2);
+		UserVo userVo = new UserVo();
+		userVo.setName("");
+		userVo.setList(list);
+		DefaultObjectResponseInfo info = new DefaultObjectResponseInfo(userVo);
+		return ResultBody.success("",info);
+	}
+
 
 }
